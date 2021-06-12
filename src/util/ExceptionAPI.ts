26d743e2a7ed046a4a5011e4c780fe3988125cba
Paulo <pauloh16.pdr@gmail.com>
2021-06-12 20:01:23 -0300

    Criar excessao para erros de api

---
+++ src/util/ExceptionAPI.ts
@@ -0,0 +1,21 @@
+const mensagens = {
+    '400': 'Dados incompletos.',
+    '401': 'Token não informado.',
+    '403': 'Você não tem autorização necessária para continuar.',
+    '404': 'Dados não encontrados.',
+    '406': 'Dados inválidos.',
+    '422': 'Informações já existem no banco de dados.',
+    '502': 'Erro ao realizar operação no banco de dados.'
+};
+
+type Mensagens = typeof mensagens;
+type codigoAPI = keyof Mensagens;
+
+export class ExceptionAPI {
+    public tipo = 'API';
+
+    constructor(
+        public cod: codigoAPI,
+        public mensagem = mensagens[cod]
+    ) {}
+};
