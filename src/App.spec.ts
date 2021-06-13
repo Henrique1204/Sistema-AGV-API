d073064195bbfe690a6c58a5bb63ba975cf1d95e
Paulo <pauloh16.pdr@gmail.com>
2021-06-13 00:31:27 -0300

    Refatorar testes de rotas

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -1,4 +1,4 @@
-import { App } from '../App';
+import { App } from './App';
 import request from 'supertest';
 
 const Server = new App();
@@ -45,6 +45,55 @@ describe('Testando Rota Status', () => {
         const res = await request(sut).post('/status').send({
             status: 'ligado',
             velocidade: 0
+        });
+
+        expect(res.statusCode).toEqual(406);
+        expect(res.body.sucesso).toBeFalsy();
+    });
+});
+
+describe('Testando Rota Sensores', () => {
+    afterEach(() => jest.clearAllMocks());
+
+    it('Deveria buscar dados dos sensores e código 200', async () => {
+        const sut = Server.express;
+        const res = await request(sut).get('/sensores');
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual(new Array());
+    });
+
+    it('Deveria retornar sucesso como true e código 201', async () => {
+        const sut = Server.express;
+
+        const res = await request(sut).post('/sensores').send({
+            nome: 'Sensor A',
+            status: false,
+            condicao: false
+        });
+
+        expect(res.statusCode).toEqual(201);
+        expect(res.body.sucesso).toBeTruthy();
+    });
+
+    it('Deveria retornar sucesso como false e código 400', async () => {
+        const sut = Server.express;
+
+        const res = await request(sut).post('/sensores').send({
+            nome: 'Sensor A'
+        });
+
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.sucesso).toBeFalsy();
+    });
+
+    it('Deveria retornar sucesso como false e código 406', async () => {
+        const sut = Server.express;
+
+        const res = await request(sut).post('/sensores').send({
+            nome: 'Sensor B',
+            status: 20,
+            condicao: 'Com problema'
         });
 
         expect(res.statusCode).toEqual(406);
