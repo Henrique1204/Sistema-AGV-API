4be51a1691497d3c5a12ee8e1587b60d87d5e30d
Paulo <pauloh16.pdr@gmail.com>
2021-06-14 06:27:35 -0300

    Corrigir camada de repositorie de Sensores

--- src/repositories/Sensores.repositorie.ts
+++ src/repositories/Sensores.repositorie.ts
@@ -14,7 +14,9 @@ export class SensoresRepositorie {
         const cache = CacheServico.getCache();
         const novoSensor = FormatarDados.formatarSensor(dados);
 
-        const indice = sensores.findIndex(({ sensor }) => sensor === novoSensor.sensor);
+        const indice = sensores.findIndex(({ sensor }) => (
+            sensor.toLowerCase() === novoSensor.sensor.toLowerCase()
+        ));
 
         if (indice === -1) sensores.push(novoSensor);
         else sensores[indice] = novoSensor;
